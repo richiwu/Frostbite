7d3c075532c2b3eaf4c5c76dc1d0ce9d33360571
yuwu94 <richard_wu94@hotmail.com>
2014-11-23 22:02:56 -0500

    new pages added

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function textHover(img, x){
+	// x.style.height = "64px";
+	img = document.getElementById("yolo").src;
+	
+}
+
+function textHoverOut(x){
+
+}
+
+function changeImage(img, txt){
+	//change image code
+	document.getElementById("yolo").src = img;
+ 	//change text code
+ 	document.getElementById("description").innerHTML = txt
+}
