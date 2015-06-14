95ec4afd1013eacd1a8c3d890f9ea25ac4a1440b
yuwu94 <richard_wu94@hotmail.com>
2015-06-13 21:11:18 -0600

    commit

--- Revamped_2/config/env/development.js
+++ Revamped_2/config/env/development.js
@@ -6,8 +6,8 @@ module.exports = {
 		title: 'FrostBite - Development Environment'
 	},
 	facebook: {
-		clientID: process.env.FACEBOOK_ID || 'APP_ID',
-		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
+		clientID: process.env.FACEBOOK_ID || '928059517235744',
+		clientSecret: process.env.FACEBOOK_SECRET || '????????',
 		callbackURL: '/auth/facebook/callback'
 	},
 	twitter: {
--- Revamped_2/public/modules/core/controllers/home.client.controller.js
+++ Revamped_2/public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,112 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		$scope.flavours = [
+			{
+				alt: "baby monkey",
+				flavId:"baby_monkey.jpg"
+			},
+			{
+				alt: "black cherry",
+				flavId:"black_cherry.jpg"
+			},
+			{
+				alt: "butterscotch ripple",
+				flavId:"butterscotch_ripple.jpg"
+			},
+			{
+				alt: "chocolate peanut butter",
+				flavId:"c_pb.jpg"
+			},
+			{
+				alt: "chocolate rasberry",
+				flavId:"c_ras.jpg"
+			},
+			{
+				alt: "chocolate chip cookie dough",
+				flavId:"cc_cookie_dough.jpg"
+			}
+		];
+
+		$scope.employees = [
+			{
+				name: "Jess",
+				major: "Electrical Engineering",
+				home_town: "Kingston",
+				fav_icecream: "Mint Oreo",
+				hobby: "nothing",
+				music: "country",
+				fact: "nothing"
+			},
+			{
+				name: "Josh",
+				major: "",
+				home_town: "",
+				fav_icecream: "",
+				hobby: "",
+				music: "",
+				fact: ""
+			},
+			{
+				name: "Sam",
+				major: "",
+				home_town: "",
+				fav_icecream: "",
+				hobby: "",
+				music: "",
+				fact: ""
+			},
+			{
+				name: "Richard",
+				major: "",
+				home_town: "",
+				fav_icecream: "",
+				hobby: "",
+				music: "",
+				fact: ""
+			},
+			{
+				name: "Emma",
+				major: "",
+				home_town: "",
+				fav_icecream: "",
+				hobby: "",
+				music: "",
+				fact: ""
+			},
+			{
+				name: "Justin",
+				major: "",
+				home_town: "",
+				fav_icecream: "",
+				hobby: "",
+				music: "",
+				fact: ""
+			},
+			{
+				name: "Jacqui",
+				major: "",
+				home_town: "",
+				fav_icecream: "",
+				hobby: "",
+				music: "",
+				fact: ""
+			},
+			{
+				name: "Jacob",
+				major: "",
+				home_town: "",
+				fav_icecream: "",
+				hobby: "",
+				music: "",
+				fact: ""
+			}
+		]
+
+
 	}
 ]);
+
+
+
